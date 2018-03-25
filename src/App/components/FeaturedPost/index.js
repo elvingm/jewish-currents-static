93c8ec98d7be372e988c6dadb4fbc70eb96584d4
elvingm <egm0921@gmail.com>
2018-03-25 03:35:22 -0400

    Featured Post styling start

--- src/App/components/FeaturedPost/index.js
+++ src/App/components/FeaturedPost/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Link } from 'react-static';
 //
+import { MONTH_NAMES } from '../../util/date';
 import './index.css';
 
 export default props => {
+  const date = new Date(props.date);
   return (
     <div className="featured-post">
       <div className="image">
@@ -11,7 +13,14 @@ export default props => {
       </div>
       <div className="details">
         <h2 dangerouslySetInnerHTML={{ __html: props.titile }} />
+        <p>
+          <span className="date">{`${MONTH_NAMES[date.getMonth()]} ${date.getDate()}`}</span>
+          <span className="author">Author Name</span>
+        </p>
         <div className="excerpt" dangerouslySetInnerHTML={{ __html: props.excerpt }} />
+        <Link to={`/${props.slug}`} className="read-more">
+          Read More
+        </Link>
       </div>
     </div>
   );

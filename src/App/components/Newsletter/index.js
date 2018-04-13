d6d43e92e12132e4ae7127a35ab0c0d415bf7291
elvingm <egm0921@gmail.com>
2018-04-13 08:47:24 -0400

    Add ctct div to Newsletter Component

--- src/App/components/Newsletter/index.js
+++ src/App/components/Newsletter/index.js
@@ -5,9 +5,8 @@ import './style.css';
 export default () => (
   <section className="newsletter-signup">
     <h2>Sign up for our mailing list</h2>
-    <form id="newsletter" className="g-border-wrap">
-      <input id="email" type="email" placeholder="Email" />
-      <input type="submit" value="Submit" className="g-button-filled" />
-    </form>
+    {/* Begin Constant Contact Inline Form Code */}
+    <div class="ctct-inline-form" data-form-id="7401b4d5-a13e-43e7-b18f-ae0b4bc82e7b" />
+    {/* End Constant Contact Inline Form Code */}
   </section>
 );

ee0b0e2ce8964c561646ee131a2a5ade9abdb66e
Elvin G. Marmol <egm0921@gmail.com>
2018-04-13 20:22:10 -0400

    Drop in constant contact inline script

--- src/App/components/Newsletter/index.js
+++ src/App/components/Newsletter/index.js
@@ -5,8 +5,6 @@ import './style.css';
 export default () => (
   <section className="newsletter-signup">
     <h2>Sign up for our mailing list</h2>
-    {/* Begin Constant Contact Inline Form Code */}
-    <div class="ctct-inline-form" data-form-id="7401b4d5-a13e-43e7-b18f-ae0b4bc82e7b" />
-    {/* End Constant Contact Inline Form Code */}
+    <div className="ctct-inline-form" data-form-id="7401b4d5-a13e-43e7-b18f-ae0b4bc82e7b" />
   </section>
 );
--- static.config.js
+++ static.config.js
@@ -122,7 +122,20 @@ export default {
         <title>{siteData.title}</title>
         <meta name="description" content={siteData.description} />
       </Head>
-      <Body>{children}</Body>
+      <Body>
+        {children}
+        {/* <!-- Begin Constant Contact Active Forms --> */}
+        <script
+          dangerouslySetInnerHTML={{ __html: `var _ctct_m = "21a5ee28f6c0fefe44a395c76e74b213";` }}
+        />
+        <script
+          id="signupScript"
+          src="//static.ctctcdn.com/js/signup-form-widget/current/signup-form-widget.min.js"
+          async
+          defer
+        />
+        {/* <!-- End Constant Contact Active Forms --> */}
+      </Body>
     </Html>
   )
 };

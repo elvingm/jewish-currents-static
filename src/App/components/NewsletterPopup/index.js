2a7c8cd692b9c34e1b1aa9d0d30ff23e07b9c1a0
elvingm <egm0921@gmail.com>
2018-04-15 20:01:17 -0400

    Add cookie to newsletter popup; Fix hyperlink styles in post to underline and be blue

--- src/App/components/NewsletterPopup/index.js
+++ src/App/components/NewsletterPopup/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactGA from 'react-ga';
 import classNames from 'classnames';
+import Cookies from 'js-cookie';
 //-
 import './style.css';
 import CloseIcon from '../../assets/img/icons/close.svg';
@@ -21,11 +22,14 @@ export default class NewsletterPopup extends React.Component {
   }
 
   showPopup = () => {
-    ReactGA.event({
-      category: 'Newsletter Popup',
-      action: 'open'
-    });
-    this.setState({ hidden: false });
+    const prompted = Cookies.get('jc:newsletter:prompted');
+    if (!prompted) {
+      ReactGA.event({
+        category: 'Newsletter Popup',
+        action: 'open'
+      });
+      this.setState({ hidden: false });
+    }
   };
 
   closePopup = () => {
@@ -34,6 +38,7 @@ export default class NewsletterPopup extends React.Component {
       action: 'close'
     });
     this.setState({ hidden: true });
+    Cookies.set('jc:newsletter:prompted', true, { expires: 1, path: '/' });
   };
 
   render() {

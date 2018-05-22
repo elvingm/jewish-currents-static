1fa9198904cd4014a900d11aed28514aadee4c31
Elvin G. Marmol <egm0921@gmail.com>
2018-05-22 13:30:11 -0400

    Strip tags from result.title during transform step

--- src/App/components/Search/Form/index.js
+++ src/App/components/Search/Form/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-static';
 import DatoCmsSearch from 'datocms-search/dist/datocms-search.base';
 import parse from 'url-parse';
 import { debounce } from 'lodash';
+import striptags from 'striptags';
 import './style.css';
 import SearchIcon from '../../../assets/img/icons/search-icon.svg';
 
@@ -26,6 +27,7 @@ export default class SearchForm extends React.Component {
       .search(query)
       .then(({ results }) => {
         const transformed = results.map(res => {
+          res.title = striptags(res.title);
           res.path = parse(res.url).pathname;
           return res;
         });
@@ -50,11 +52,9 @@ export default class SearchForm extends React.Component {
           {this.state.results.map((result, idx) => (
             <div className="result" key={`result-${result.path}-${idx}`}>
               <h2 className="title">
-                <Link
-                  to={result.path}
-                  onClick={this.props.onResultClick}
-                  dangerouslySetInnerHTML={{ __html: result.title }}
-                />
+                <Link to={result.path} onClick={this.props.onResultClick}>
+                  {result.title}
+                </Link>
               </h2>
               <div className="body" dangerouslySetInnerHTML={{ __html: result.body }} />
             </div>

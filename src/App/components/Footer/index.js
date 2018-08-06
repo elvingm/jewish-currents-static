5bbc32b6816c133c993c56ec9a41d5ed18c3aa5e
donato <cruzmdonato@gmail.com>
2018-08-06 11:27:20 -0400

    address PR feedback

--- src/App/components/Footer/index.js
+++ src/App/components/Footer/index.js
@@ -3,13 +3,14 @@ import { Link, withRouteData } from 'react-static';
 //
 import './style.css';
 import { toRGBString } from '../../util/helpers';
+import LAReviewOfBooksLogo from '../../../../public/img/la-review-of-books.png';
 
 export default withRouteData(({ footerColor }) => {
   // default background color to white if none provided to route
   footerColor = footerColor || { red: 255, green: 255, blue: 255 };
   return (
     <footer id="site-footer" style={{ backgroundColor: toRGBString(footerColor) }}>
-      <div className="footer-wrapper">
+      <div className="g-thick-border footer-content-wrap">
         <ul>
           <li>Jewish Currents</li>
           <li>
@@ -19,7 +20,13 @@ export default withRouteData(({ footerColor }) => {
             <Link to="/about/privacy-policy">Privacy Policy</Link>
           </li>
         </ul>
-        <p className="la-review-of-books">A Los Angeles Review of Books Affiliate</p>
+        <p>
+          <img
+            className="la-review-of-books-logo"
+            src={LAReviewOfBooksLogo}
+            alt="Los Angeles Review of Books Affiliate Logo"
+          />LA Review Of Books Channel
+        </p>
       </div>
     </footer>
   );
